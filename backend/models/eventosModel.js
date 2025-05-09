e2939a0bfda0fa8dd2d4738bfad209ffbfd769aa
Pame <pgendrop@anahuac.mx>
2025-05-08 20:35:26 -0600

    chore: update package.json to include new dependencies and scripts
    - Removed main entry point and test script.
    - Added start and server scripts for backend.
    - Included new dependencies: bcrypt, body-parser, colors, cors, dotenv, express, jsonwebtoken, module, mongodb.
    - Updated mongoose version.
    - Updated devDependencies with nodemon and adjusted @types/node version.
    - Specified Node.js engine version.

--- backend/models/eventosModel.js
+++ backend/models/eventosModel.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
-const habitacionesSchema = mongoose.Schema({ 
+const eventosSchema = mongoose.Schema({ 
     title: {
-        type : String,
-        required: [true, "El titulo del evento es obligatorio"]
+        type: String,
+        required: [true, "El título del evento es obligatorio"]
     },
     start: {
-        type : Date,
+        type: Date,
         required: [true, "La fecha de inicio es obligatoria"]
     },
     end: {
-        type : Date,
+        type: Date,
         required: [true, "La fecha de finalización es obligatoria"]
     }
-    
-},{
+}, {
     timestamps: true
 });
 
-module.exports = mongoose.model("Eventos", eventosSchema, `Eventos`);
+module.exports = mongoose.model("Eventos", eventosSchema, "Eventos");
--- backend/routes/eventosRoutes.js
+++ backend/routes/eventosRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getHabitaciones, createHabitacion, updateHabitacion, deleteHabitacion } = require('../controllers/habitacionesControllers');
 const { getEventos, createEvento, updateEvento, deleteEvento } = require('../controllers/eventosControllers');
 
-// Obtenemos las habitaciones
+// Obtenemos los eventos
 router.get('/', getEventos);
-// Creamos una habitación
+// Creamos un evento
 router.post('/', createEvento);
-// Modificamos la disponibilidad de una habitación
+// Modificamos un evento
 router.put('/', updateEvento);
-// Eliminamos una habitación
+// Eliminamos un evento
 router.delete('/', deleteEvento);
 
 module.exports = router;
